59676393eb81629b87d1b6b0c06dd5592c8577af
rileyrichter <ben@webflow.com>
2022-07-27 12:34:46 -0500

    updated to show confirmation text

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const baseUrl = "https://bparker.api.stdlib.com/scrape-xml@dev/";
 selectOne = document.querySelector("#pageone");
 selectTwo = document.querySelector("#pagetwo");
+submit = document.querySelector("#submit");
+exName = document.querySelector("#exname");
+exStart = document.querySelector("#exstart");
+exEnd = document.querySelector("#exend");
+exUrl1 = document.querySelector("#exurl1");
+exUrl2 = document.querySelector("#exurl2");
+experimentName = querySelector("experiment-name");
+experimentStart = querySelector("start-date");
+experimentEnd = querySelector("end-date");
+experimentGroupOne = document.querySelector("#pageone");
+experimentGroupTwo = document.querySelector("#pagetwo");
 pageList = [];
 objectList = [];
 
@@ -42,3 +53,11 @@ window.addEventListener("load", (event) => {
       console.table(objectList);
     });
 });
+
+submit.onClick = function () {
+  exName.innerText = experimentName.value;
+  exStart.innerText = experimentStart.value;
+  exEnd.innerText = experimentEnd.value;
+  exUrl1.innerText = experimentGroupOne.value;
+  exUrl2.innerText = experimentGroupTwo.value;
+};
